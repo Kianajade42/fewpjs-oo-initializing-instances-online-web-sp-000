caea745bb2fd2f9f3482e4f6dd949eda90102895
Kiana Long <kianajade42.kl@gmail.com>
2021-04-22 14:43:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class Dinner {
   constructor(salad,soup,entree,dessert){
     this.salad=salad;
     this.soup=soup;
-    this.drink=drink;
+    this.entree=entree;
     this._dessert=dessert;
   }
 }
